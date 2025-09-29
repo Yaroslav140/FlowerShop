b559e5270e4a46dbca16747f26e2508f5eab066d
yarik <paramonovjaroslav1245@gmail.com>
2025-09-29 08:51:04 +0300

    update front

--- obj/Debug/net9.0/FlowerShop.Web.AssemblyInfo.cs
+++ obj/Debug/net9.0/FlowerShop.Web.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlowerShop.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7bb7b16e1be7bd38a1f2e01ba310353194d1034")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+746ebd578518759c3e050da00bc78750c1acd56d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlowerShop.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlowerShop.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
