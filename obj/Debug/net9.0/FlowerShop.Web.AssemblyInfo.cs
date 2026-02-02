cb0e88ff252e25a71084fe0a539a19fd6ba35e74
YA <paramonovjaroslav1245@gmail.com>
2026-02-02 08:06:37 +0300

    update button cancel

--- obj/Debug/net9.0/FlowerShop.Web.AssemblyInfo.cs
+++ obj/Debug/net9.0/FlowerShop.Web.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlowerShop.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fdc03858d437c80044a2dc641e03ed726cddeda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a80f4e162c8d0f5daecfc4ae69aa74f0b20af55")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlowerShop.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlowerShop.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
